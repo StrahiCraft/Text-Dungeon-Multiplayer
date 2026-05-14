6c902bccb4ea5416c2d42ca01459c70e374d6a4e
StrahiCraft <strahi04@gmail.com>
2026-05-14 10:27:10 +0200

    moved login rendering to separate function

--- src/main/java/client/game/game_state/LoginState.java
+++ src/main/java/client/game/game_state/LoginState.java
@@ -11,16 +11,7 @@ import java.util.Scanner;
 public class LoginState extends GameState {
     @Override
     public void updateState(Scanner input) {
-        TextRenderer.printText("Welcome to the login screen, type in " + Color.getColor("green") + "login" + Color.resetColor() +
-                " <" + Color.getColor("yellow") + "username" + Color.resetColor() + "> <" + Color.getColor("yellow") +
-                "password" + Color.resetColor() + "> to log in to your existing account.");
-
-        TextRenderer.printText("If you don't have an account, create one by typing in " + Color.getColor("blue") + "register" + Color.resetColor() +
-                " <" + Color.getColor("yellow") + "username" + Color.resetColor() + "> <" + Color.getColor("yellow") +
-                "password" + Color.resetColor() + ">");
-
-        TextRenderer.printText("If you wish to play offline, type in " + Color.getColor("yellow") + "offline");
-        TextRenderer.printText("Type in " + Color.getColor("red") + "quit" + Color.resetColor() + " to quit the game");
+        renderLoginScreen();
 
         String command = input.nextLine();
         String[] commandParameters = command.split(" ");
@@ -54,5 +45,18 @@ public class LoginState extends GameState {
                 Game.changeState(new DefaultGameState());
             }
         }
+    }
+
+    private void renderLoginScreen(){
+        TextRenderer.printText("Welcome to the login screen, type in " + Color.getColor("green") + "login" + Color.resetColor() +
+                " <" + Color.getColor("yellow") + "username" + Color.resetColor() + "> <" + Color.getColor("yellow") +
+                "password" + Color.resetColor() + "> to log in to your existing account.");
+
+        TextRenderer.printText("If you don't have an account, create one by typing in " + Color.getColor("blue") + "register" + Color.resetColor() +
+                " <" + Color.getColor("yellow") + "username" + Color.resetColor() + "> <" + Color.getColor("yellow") +
+                "password" + Color.resetColor() + ">");
+
+        TextRenderer.printText("If you wish to play offline, type in " + Color.getColor("yellow") + "offline");
+        TextRenderer.printText("Type in " + Color.getColor("red") + "quit" + Color.resetColor() + " to quit the game");
     }
 }
